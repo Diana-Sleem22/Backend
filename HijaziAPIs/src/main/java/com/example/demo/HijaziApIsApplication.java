3c80e15a66451d7635fbdadbed12a9092fb3a72c
Diana-Sleem22 <109549014+Diana-Sleem22@users.noreply.github.com>
2022-12-03 13:58:14 +0200

    initial commit

---
+++ HijaziAPIs/src/main/java/com/example/demo/HijaziApIsApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class HijaziApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HijaziApIsApplication.class, args);
+	}
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	  return application.sources(HijaziApIsApplication.class);
+	 }
+	 
+}
